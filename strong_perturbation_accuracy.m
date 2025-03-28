838e27030489bb6150fbbc124fbbfa88ffc6398e
Yubei83 <yeyubei83@gmail.com>
2025-03-28 02:09:14 +0000

    Update strong_perturbation_accuracy.m

--- strong_perturbation_accuracy.m
+++ strong_perturbation_accuracy.m
@@ -1,12 +1,1 @@
-strategies = {'Baseline', 'Basic Aug', 'Advanced Aug', 'Selective Aug'};
-accuracy = [66.75, 49.51, 51.42, 63.37];
 
-figure;
-bar(accuracy, 0.6);
-set(gca, 'XTickLabel', strategies, 'FontSize', 12);
-ylabel('Accuracy under Strong Perturbation (%)');
-title('Model Robustness under Strong Perturbation');
-ylim([0 80]);
-grid on;
-
-saveas(gcf, 'strong_perturbation_accuracy.png');
